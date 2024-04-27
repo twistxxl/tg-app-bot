1027854872638c28c81e822fe5c8426b54b729a0
twistxxl <glade1337@mail.ru>
2024-04-27 22:15:06 +0300

    init

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -18,11 +18,11 @@ const Form  = () => {
         tg.sendData(JSON.stringify(data))
     }, [])
     useEffect(() => {
-            tg.WebApp.onEvent('mainButtonClicked', onSendData)
+            tg.onEvent('mainButtonClicked', onSendData)
         return () => {
-            tg.WebApp.offEvent('mainButtonClicked', onSendData)  
+            tg.offEvent('mainButtonClicked', onSendData)  
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
