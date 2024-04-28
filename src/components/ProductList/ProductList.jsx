fbd7e6eaf5c7e02d074dcca571b3270389cc361d
twistxxl <glade1337@mail.ru>
2024-04-28 11:14:18 +0300

    m

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,20 +5,20 @@ import {useTelegram} from '../../hooks/useTelegram';
 
 
 const products = [
-    {id: '1', title: 'Солями', price: '500', description: '30cm'},
-    {id: '2', title: 'Грибная', price: '500', description: '30cm'},
-    {id: '3', title: 'Ананасовая', price: '500', description: '30cm'},
-    {id: '4', title: 'СолямиLite', price: '300', description: '20cm'},
-    {id: '5', title: 'Мясная', price: '560', description: '30cm'},
-    {id: '6', title: 'Охотнячая', price: '600', description: '30cm'},
-    {id: '7', title: 'Свиняча', price: '1500', description: '40cm'},
-    {id: '8', title: 'Маргарита', price: '250', description: '20cm'},
-    {id: '9', title: '4Сыра', price: '400', description: '30cm'},
+    {id: '1', title: 'Солями', price: 500, description: '30cm'},
+    {id: '2', title: 'Грибная', price: 500, description: '30cm'},
+    {id: '3', title: 'Ананасовая', price: 500, description: '30cm'},
+    {id: '4', title: 'СолямиLite', price: 300, description: '20cm'},
+    {id: '5', title: 'Мясная', price: 560, description: '30cm'},
+    {id: '6', title: 'Охотнячая', price: 600, description: '30cm'},
+    {id: '7', title: 'Свиняча', price: 1500, description: '40cm'},
+    {id: '8', title: 'Маргарита', price: 250, description: '20cm'},
+    {id: '9', title: '4Сыра', price: 400, description: '30cm'},
 ]
 
-const getTotalPrice = (products = []) => {
-    return products.reduce((acc, products) => {
-        return acc += products.price
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
     } , 0)
 }
 
